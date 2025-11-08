c28d2d1095980f88815d7f2bfb58f053c2d820d4
xdesign-jessiezhu <jessie.zhu@xdesign.com>
2025-11-08 16:08:52 +0000

    chore: add Google AdSense script to layout head

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google AdSense */}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1802611351208013"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body>
         {/* Basic SSR shell to avoid empty HTML responses */}
         <div id="app-shell">
